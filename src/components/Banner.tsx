1e665e5a4c8c37a945492bd62e2d5b1fb4ef5dd6
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-10-06 23:33:52 +0200

    Made hero/banner into a video

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,8 @@
 const Banner = () => {
     return(
         <section 
-        style={{ backgroundImage: "url('/viego/Banner_Viego.jpg')" }}
-        className="relative h-[60vh] w-9/10 bg-teal-800 m-auto rounded-xl border-2 border-teal-900 bg-right lg:bg-top bg-cover">
+        className="relative h-[60vh] w-9/10 bg-teal-800 m-auto rounded-xl border-2 border-teal-900 overflow-hidden shadow-xl shadow-teal-100">
+            <video autoPlay loop muted playsInline src="/videos/ViegoBanner.mp4"></video>
             <button className="absolute right-7 lg:right-20 bottom-10 bg-teal-100 border-2 border-teal-900 rounded p-2 h-15 w-30">Button</button>
         </section>
     )
