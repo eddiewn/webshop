1e7e1ec3448141957f724a2fb626c4489be7cf2d
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-10-08 17:35:12 +0200

    Added a pretty shitty footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,58 @@
 const Footer = () => {
-    return(
-        <footer>
-            Hello im footer
+    return (
+        <footer className="relative mb-auto w-screen h-100 bg-[var(--secondary-bg)]">
+            <section className="flex justify-around">
+                <section className="w-1/5">
+                    <h2 className="text-center">About us</h2>
+                    <p>
+                        The Viego website exists to celebrate and showcase the
+                        lore, design, and aesthetic of Viego, the Ruined King
+                        from League of Legends. It offers fans detailed insights
+                        into his story, abilities, and skins while serving as a
+                        creative hub for artwork, game updates, and merchandise
+                        inspired by his haunting, tragic character.
+                    </p>
+                </section>
+
+                <section className="w-1/5">
+                    <h2 className="text-center">Follow Us</h2>
+                    <ul>
+                        <li>
+                            <a href="https://twitter.com">
+                                Twitter
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://facebook.com"
+                            >
+                                Facebook
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://instagram.com"
+                            >
+                                Instagram
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+                <section className="w-1/5">
+                <h3 className="text-center">Team</h3>
+                    <ul>
+                        <li>Lead Developer: Eddie Wiik Nilsson</li>
+                        <li>Designer: Eddie Wiik Nilsson</li>
+                    </ul>
+                </section>
+                            <section className="w-1/5">
+            <h2>Contact us</h2>
+            </section>
+            </section>
+
+            <small className="absolute bottom-0 left-1/2 -translate-x-1/2">@2025 Viego Webshop | All Rights reserved</small>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
