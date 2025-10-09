44db77334d9c7debbbd95d96cb80668dcd0c6237
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-10-09 23:15:52 +0200

    Footer updates.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="relative mb-auto w-screen h-100 bg-[var(--secondary-bg)]">
-            <section className="flex justify-around">
-                <section className="w-1/5">
+        <footer className="relative w-screen bg-[var(--secondary-bg)]">
+            <section className="flex flex-col md:flex-row items-center md:justify-around gap-20 mb-20">
+                <section className="w-4/5 md:w-1/5">
                     <h2 className="text-2xl text-center">Follow Us</h2>
                     <ul>
                         <li>
@@ -16,19 +16,19 @@ const Footer = () => {
                         </li>
                     </ul>
                 </section>
-                <section className="w-1/5">
+                <section className="w-4/5 md:w-1/5">
                     <h3 className="text-2xl text-center">Team</h3>
                     <ul>
                         <li>Lead Developer: Eddie Wiik Nilsson</li>
                         <li>Designer: Eddie Wiik Nilsson</li>
                     </ul>
                 </section>
-                <section className="w-1/5">
+                <section className="w-4/5 md:w-1/5">
                     <h2 className="text-2xl ">Contact us</h2>
                 </section>
             </section>
 
-            <small className="absolute bottom-0 left-1/2 -translate-x-1/2">
+            <small className="absolute text-center bottom-0 left-1/2 -translate-x-1/2">
                 @2025 Viego Webshop | All Rights reserved
             </small>
         </footer>
